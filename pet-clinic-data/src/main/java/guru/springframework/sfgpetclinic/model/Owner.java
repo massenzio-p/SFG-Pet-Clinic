6fbb47b24f263bb4f47b9cb8e2f18a4317101ea6
Max Pestov <pestov.maxik@gmail.com>
2021-12-28 16:06:08 +0500

    added owner properties. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Set;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @AllArgsConstructor
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
 
     private Set<Pet> pets;
 }
