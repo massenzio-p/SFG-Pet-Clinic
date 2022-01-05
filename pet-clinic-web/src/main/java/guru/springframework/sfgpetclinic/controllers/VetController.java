b914260cfb5924bbb2b2dea1262a49eb54350da2
Max Pestov <pestov.maxik@gmail.com>
2022-01-05 11:09:25 +0500

    Menu broken links fixed, closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -7,17 +7,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@RequestMapping("/vets")
 @Controller
 public class VetController {
     private final VetService vetService;
 
     @RequestMapping({
-            "",
-            "/index",
-            "/index.html"})
+            "vets",
+            "vets/index",
+            "vets/index.html",
+            "vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
 }
