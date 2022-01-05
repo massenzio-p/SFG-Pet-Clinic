f545717d0e8b62b5dc655089b1cda7ef3fb87de6
Max Pestov <pestov.maxik@gmail.com>
2022-01-05 10:08:46 +0500

    PetTypes added to bootstrap, closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,12 +3,14 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Owner extends Person {
     private String address;
     private String city;
